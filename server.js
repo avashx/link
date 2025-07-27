31eb75904d782dd2a17a5bcc3118b1c2705c9ac1
avashx <amanvash.official@gmail.com>
2025-07-27 14:33:36 +0530

    fix of CORS

--- server.js
+++ server.js
@@ -53,6 +53,21 @@ async function initializeStorage() {
 }
 
 const app = express();
+
+// Add CORS headers for all requests
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 app.use(express.static('public'));
 // Serve screenshot images from root directory
 app.use('/screenshots', express.static('.', {
